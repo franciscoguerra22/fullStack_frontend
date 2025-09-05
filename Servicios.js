e795bfeab68a89a4ac1706f4088026f553a4ccc6
Francisco Guerra <fra.guerram@duocuc.cl>
2025-09-04 21:59:15 -0400

    first commit

---
+++ Servicios.js
@@ -0,0 +1,58 @@
+const servicios = [
+    {
+        id: 1,
+        titulo: "Personaje y fondo Full detallados",
+        descripcion: "Alto nivel de renderizado en texturas (piel, metal, tela, energía), sombras precisas y efectos atmosféricos. Ideal para portadas, material promocional o arte conceptual final.",
+        precio: "200.000",
+        imagen: "img/ilustracion_full.JPG"
+    },
+    {
+        id: 2,
+        titulo: "Ilustración full detallada con fondo simple",
+        descripcion: "Personaje principal altamente detallado: Anatomía precisa, expresión facial definida, texturas realistas (piel, ropa, armas, alas, etc.)",
+        precio: "120.000",
+        imagen: "img/full_Ilustracion_fondo_simple.jpeg"
+    },
+    {
+        id: 3,
+        titulo: "Ilustración y fondo detallado",
+        descripcion: "Composición narrativa compleja: Escenas con múltiples personajes, criaturas, arquitectura y elementos ambientales que cuentan una historia visual.",
+        precio: "150.000",
+        imagen: "img/fondo detallado.jpeg"
+    },
+    {
+        id: 4,
+        titulo: "Personaje + fondo-Pintura simple",
+        descripcion: "Personaje central con pintura simple: Diseño anatómico preciso, expresión facial intensa, múltiples extremidades o elementos únicos (como armas, joyería, coronas, etc.). Ideal para representar dioses, guerreros, entidades mágicas o figuras legendarias",
+        precio: "90.000",
+        imagen: "img/personaje_pintura_simple.jpeg"
+    },
+    {
+        id: 5,
+        titulo: "Diseño de personaje-Full body",
+        descripcion: "Representación anatómica precisa desde la cabeza hasta los pies. Se incluyen elementos como vestimenta, accesorios, armas, postura y expresión facial que definen la personalidad y rol del personaje",
+        precio: "110.000",
+        imagen: "img/full_body.jpg"
+    },
+    {
+        id: 6,
+        titulo: "Diseño de personaje-Mid body",
+        descripcion: "Representación desde la cabeza hasta la cintura o muslos: Se enfoca en los rasgos faciales, torso, brazos y parte superior de las piernas. Ideal para mostrar expresividad, vestimenta, accesorios y postura sin abarcar el cuerpo completo.",
+        precio: "100.000",
+        imagen: "img/mid_body_final.png"
+    },
+    {
+        id: 7,
+        titulo: "Diseño de personaje, estilizado, con pintura simple",
+        descripcion: "Estilo estilizado y simplificado: Se prioriza la forma, silueta y personalidad del personaje sobre el realismo. Las proporciones pueden ser exageradas o caricaturescas para enfatizar rasgos distintivos.",
+        precio: "70.000",
+        imagen: "img/simple_estilizado.png"
+    },
+    {
+        id: 8,
+        titulo: "Concept art de personajes",
+        descripcion: "Diseño exploratorio y expresivo: El personaje se presenta en una pose dinámica o natural, con énfasis en la silueta, proporciones y actitud. Ideal para representar guerreros, magos, criaturas o cualquier figura fantástica en etapa de desarrollo visual",
+        precio: "60.000",
+        imagen: "img/concept_art.png"
+    }
+];
